8a4088674ee8d34746b3933e1fbccb37d8033bfd
Romain Avalle <romain.avalle@lg2.com>
2023-07-10 10:00:55 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#nuxtmodule-consent-gtm-nuxt3
+
+to be placed before gtm module
+
+```
+modules: [
+    '@lg2/nuxtmodule-consent-gtm-nuxt3',
+    '@nuxtjs/gtm',
+],
+consentGTM: { localStorageItem: 'storedConsentLocale' },
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+import { defineNuxtModule } from '@nuxt/kit'
+function  addConsent(options) {
+  const scripts = options.head?.script || [];
+  scripts.push({
+    hid: options.scriptId,
+    innerHTML: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      if(!window.isFlag){ // weird reset on first page change
+          window.isFlag = true;
+          if (localStorage.getItem('${options.localStorageItem}') === null) {
+          gtag('consent', 'default', {
+            'ad_storage': 'denied',
+            'analytics_storage': 'denied'
+          });
+        }else{
+            var consent = JSON.parse(localStorage.getItem('${options.localStorageItem}'))
+            var consentMode = {
+              ad_storage: consent.ad_storage ? 'granted' : 'denied',
+              analytics_storage: consent.analytics_storage ? 'granted' : 'denied',
+              functionality_storage: consent.functionality_storage
+                ? 'granted'
+                : 'denied',
+              personalization_storage: consent.personalization_storage
+                ? 'granted'
+                : 'denied',
+              security_storage: consent.security_storage ? 'granted' : 'denied',
+            }
+            gtag('consent', 'default', consentMode)
+          }
+        }
+        `,
+  });
+  options.head.__dangerouslyDisableSanitizersByTagID =
+    options.head.__dangerouslyDisableSanitizersByTagID || {};
+  options.head.__dangerouslyDisableSanitizersByTagID[options.scriptId] = [
+    "innerHTML",
+  ];
+}
+export default defineNuxtModule({
+  setup (options, nuxt) {
+
+   const opts = {
+    localStorageItem:
+    nuxt.options.consentGTM?.localStorageItem || "storedConsentLocale",
+    ...nuxt.options.app,
+    scriptId: "consent-script",
+  };
+  nuxt.options.runtimeConfig.public.consent = opts.localStorageItem;
+  !process.client && addConsent(opts);
+  }
+})
+
